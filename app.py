4679661a36863342bdfe1f33ea1979d842cf44ca
StephenFilippone <stephenandrewf@gmail.com>
2023-01-11 17:20:19 -0300

    update 3

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ import streamlit
 df_escolasSP = pd.read_csv("escolas_2021_SP.csv", sep=',', encoding = "ascii")
 
 df_escolasSP
+
+temp = df_escolasSP.dropna(subset=['QT_DESKTOP_ALUNO'])
+temp = temp[temp['QT_DESKTOP_ALUNO']!=88888]
+temp = temp[temp['QT_DESKTOP_ALUNO']!=0]
+
+fig = px.scatter_mapbox(temp, lat='Latitude', lon='Longitude', size='QT_DESKTOP_ALUNO',
+                        center=dict(lat=-23.55, lon=-46.6), zoom=9.5,
+                        mapbox_style="stamen-terrain", hover_name='NO_ENTIDADE', hover_data=['QT_MAT_BAS_11_14','QT_MAT_BAS_15_17'])
+
+
+fig.show()
