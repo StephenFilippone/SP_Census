d4981a53093f8f77ad81793623cd8706e4166e00
StephenFilippone <stephenandrewf@gmail.com>
2023-01-13 15:35:26 -0300

    update mapbox

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
 
 df_escolasSP = pd.read_csv("escolas_2021_SP.csv", sep=',', encoding = "ascii")
@@ -49,22 +50,57 @@ with tab2:
     st.caption('QT_MAT_BAS_15_17 representa o número de Matrículas na Educação Básica entre 15 e 17 anos de idade')
 
 with tab3:
-    st.header("Matriculas 11-14")
+    temp3 = df_escolasSP.dropna(subset=['QT_MAT_BAS_15_17'])
 
-    temp3 = df_escolasSP.dropna(subset=['QT_MAT_BAS_11_14'])
-    
-    fig3 = px.scatter_mapbox(temp3, lat='Latitude', lon='Longitude', size='QT_MAT_BAS_11_14',
-                        center=dict(lat=-23.55, lon=-46.6), zoom=9.5,
-                        mapbox_style="stamen-terrain",  hover_name='NO_ENTIDADE', hover_data=dict(
-                                    QT_MAT_BAS_15_17=True, 
-                                    Latitude=False, 
-                                    Longitude=False))
-    fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    mapbox_access_token = open("TUMO.mapbox_token").read()
+
+    fig3 = go.Figure(
+                go.Scattermapbox(
+                lat=temp3["Latitude"],
+                lon = temp3['Longitude'],
+                mode='markers',
+                marker=go.scattermapbox.Marker(
+                size=temp3['QT_MAT_BAS_15_17']*25/(temp3['QT_MAT_BAS_15_17'].max())
+            ),
+    ))
+
+
+    fig3.update_layout(mapbox=dict(
+            accesstoken=mapbox_access_token,
+            center=dict(
+                lat=-23.55,
+                lon=-46.6), zoom=10))
+
+    fig3.add_trace(go.Scattermapbox(
+            lat=[-23.557162590546906, -23.559738635227205],
+            lon=[-46.68954674702313, -46.69842457794723],
+            mode='markers+text',
+            marker={'size':10, 'symbol':['star','star']},
+            text = ["42", "TUMO",],textposition = "bottom right"
+            
+    ))
 
     st.plotly_chart(fig3, use_container_width=True, sharing="streamlit", theme=None)
     st.caption('O mapa mostra a quantidade de desktop que tem cada escola. O tamanho da bolha representa a quantidade de desktops.')
     st.caption('QT_MAT_BAS_11_14 representa o número de Matrículas na Educação Básica entre 11 e 14 anos de idade')
     st.caption('QT_MAT_BAS_15_17 representa o número de Matrículas na Educação Básica entre 15 e 17 anos de idade')
+
+    # st.header("Matriculas 11-14")
+
+    # temp3 = df_escolasSP.dropna(subset=['QT_MAT_BAS_11_14'])
+    
+    # fig3 = px.scatter_mapbox(temp3, lat='Latitude', lon='Longitude', size='QT_MAT_BAS_11_14',
+    #                     center=dict(lat=-23.55, lon=-46.6), zoom=9.5,
+    #                     mapbox_style="stamen-terrain",  hover_name='NO_ENTIDADE', hover_data=dict(
+    #                                 QT_MAT_BAS_15_17=True, 
+    #                                 Latitude=False, 
+    #                                 Longitude=False))
+    # fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    # st.plotly_chart(fig3, use_container_width=True, sharing="streamlit", theme=None)
+    # st.caption('O mapa mostra a quantidade de desktop que tem cada escola. O tamanho da bolha representa a quantidade de desktops.')
+    # st.caption('QT_MAT_BAS_11_14 representa o número de Matrículas na Educação Básica entre 11 e 14 anos de idade')
+    # st.caption('QT_MAT_BAS_15_17 representa o número de Matrículas na Educação Básica entre 15 e 17 anos de idade')
 
 
 
