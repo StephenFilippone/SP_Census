30ee963fef3fda33494f8e022d73cd5531c84a4a
StephenFilippone <stephenandrewf@gmail.com>
2023-02-01 15:14:52 -0300

    new map

--- app.py
+++ app.py
@@ -12,17 +12,47 @@ tab1, tab2, tab3 = st.tabs(["Desktops", "Comp Portatil", "Matriculas 11-14"])
 with tab1:
     st.header("Número de Desktops")
    
-    temp1 = df_escolasSP.dropna(subset=['QT_DESKTOP_ALUNO'])
-    temp1 = temp1[temp1['QT_DESKTOP_ALUNO']!=88888]
-    temp1 = temp1[temp1['QT_DESKTOP_ALUNO']!=0]
+    temp1 = df_escolasSP.dropna(subset=['QT_MAT_BAS_15_17'])
 
-    fig1 = px.scatter_mapbox(temp1, lat='Latitude', lon='Longitude', size='QT_DESKTOP_ALUNO',
-                        center=dict(lat=-23.55, lon=-46.6), zoom=9.5,
-                        mapbox_style="stamen-terrain",  hover_name='NO_ENTIDADE', hover_data=dict(QT_MAT_BAS_11_14=True,
-                                    QT_MAT_BAS_15_17=True, 
-                                    Latitude=False, 
-                                    Longitude=False))
-    fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    mapbox_access_token = open("TUMO.mapbox_token").read()
+
+    fig1 = go.Figure(
+                go.Scattermapbox(
+                lat=temp1["Latitude"],
+                lon = temp1['Longitude'],
+                mode='markers',
+                hovertext=temp1['NO_ENTIDADE'],
+                customdata=temp1['QT_MAT_BAS_15_17'],
+                showlegend=False,
+                marker=go.scattermapbox.Marker(
+                size=temp1['QT_MAT_BAS_15_17']*35/(temp1['QT_MAT_BAS_15_17'].max()),
+                color=temp1['COMPS_PER_STUDENT'],
+                cmax=1,
+                cmin=0,
+                colorbar=dict(
+                title="Comp/Aluno"
+            ),
+            colorscale="portland")
+    ))
+
+
+    fig1.update_layout(mapbox=dict(
+            accesstoken=mapbox_access_token,
+            style="outdoors",
+            center=dict(
+                lat=-23.55,
+                lon=-46.6), zoom=10),margin={"r":0,"t":0,"l":0,"b":0},)
+
+    fig1.add_trace(go.Scattermapbox(
+            lat=[-23.557162590546906, -23.559738635227205],
+            lon=[-46.68954674702313, -46.69842457794723],
+            mode='markers+text',
+            showlegend=False,
+            marker={'size':10, 'symbol':['star','star']},
+            text = ["42", "TUMO",],textposition = "bottom right"
+            
+    ))
+
 
     st.plotly_chart(fig1, use_container_width=True, sharing="streamlit", theme=None)
     st.caption('O mapa mostra a quantidade de desktop que tem cada escola. O tamanho da bolha representa a quantidade de desktops.')
@@ -109,5 +139,8 @@ with tab3:
 
 
 #st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+
+
+
 st.text('Dados do Censo Escolar 2021')
 df_escolasSP
